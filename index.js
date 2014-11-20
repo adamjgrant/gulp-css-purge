18d8c1291c6683ba5e8e01c92937b039ed03948e
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-11-20 16:42:47 -0500

    filling it out a little more.

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ const PLUGIN_NAME = 'gulp-css-purge';
 
 function gulpCSSPurge() {
   var stream = through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      // File is null, do nothing.
+    }
+
+    if (file.isBuffer()) {
+      file.contents = file.contents;
+    }
   
+    if (file.isStream()) {
+      file.contents = file.contents;
+    }
+
+    this.push(file);
+
+    return cb();
   })
   return stream;
 }
+
+module.exports = gulpCSSPurge;
 
 
